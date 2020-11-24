30f6580c8116d2358db3e12dae1ac824a267ad4e
Inusah-Said <saidinusah29@gmail.com>
2020-11-24 04:11:20 +0000

    logic for send button or mic button

--- src/components/Chat/ChatBottom.js
+++ src/components/Chat/ChatBottom.js
@@ -1,22 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import EmojiEmotionsIcon from '@material-ui/icons/EmojiEmotions';
 import { IconButton } from '@material-ui/core';
 import SendIcon from '@material-ui/icons/Send';
 import MicNoneIcon from '@material-ui/icons/MicNone';
 import './Chat.css';
 const ChatBottom = () => {
+	const [textMessage, setTextMessage] = useState('');
+	const handleChange = (e) => {
+		setTextMessage(e.target.value);
+	};
 	return (
 		<div className="chat_bottom">
 			<IconButton>
 				<EmojiEmotionsIcon style={{ fontSize: 30 }} />
 			</IconButton>
-			<input placeholder="Type a message" autofocus />
-			<IconButton>
-				<SendIcon style={{ fontSize: 30, color: '#25D366' }} />
-			</IconButton>
-			<IconButton>
-				<MicNoneIcon style={{ fontSize: 30, color: 'gray' }} />
-			</IconButton>
+			<input placeholder="Type a message" autofocus onChange={handleChange} />
+
+			{textMessage?.length === 0 ? (
+				<IconButton>
+					<MicNoneIcon style={{ fontSize: 30, color: 'gray' }} />
+				</IconButton>
+			) : (
+				<IconButton>
+					<SendIcon style={{ fontSize: 30, color: '#25D366' }} />
+				</IconButton>
+			)}
 		</div>
 	);
 };
