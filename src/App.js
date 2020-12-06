9f807681e4d76dafa6dca478992fad4e13b96199
Inusah-Said <saidinusah29@gmail.com>
2020-12-06 22:18:14 +0000

    implemented real time notifs and axios instance

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
 import Chat from './components/Chat/Chat';
 import SideBar from './components/SideBar/SideBar';
 import './App.css';
+import Pusher from 'pusher-js';
 function App() {
+	useEffect(() => {
+		//put the pusher config here then life goes on
+		const pusher = new Pusher('1b590fcb4646c963f34d', {
+			cluster: 'mt1',
+		});
+
+		var channel = pusher.subscribe('messages');
+		channel.bind('inserted', function (data) {
+			alert(JSON.stringify(data));
+		});
+	}, []);
 	return (
 		<div className="app">
 			<div className="app_body">
